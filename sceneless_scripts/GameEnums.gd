1d77d65bfca961f0b8bf522d2d79b83a67cdd985
Jack Grimes <flangojack@hotmail.com>
2022-01-10 11:00:13 +0000

    moving the GameEnums class into a relevant folder

---
+++ sceneless_scripts/GameEnums.gd
@@ -0,0 +1,7 @@
+extends Node
+
+class_name GameEnums
+
+enum LetterState {NONE, HIDDEN, CORRECT_POSITION, INCORRECT_POSITION}
+enum DifficultyLevel {EASY, MEDIUM, HARD}
+enum GameRounds {FIRST_ROUND, SECOND_ROUND, THIRD_ROUND}
