bba94d308f6274482b01239c324aff705caaf72c
Jack Grimes <flangojack@hotmail.com>
2022-01-10 11:00:58 +0000

    Removing the old file after moving it.

--- GameEnums.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-class_name GameEnums
-
-enum LetterState {NONE, HIDDEN, CORRECT_POSITION, INCORRECT_POSITION}
-enum DifficultyLevel {EASY, MEDIUM, HARD}
-enum GameRounds {FIRST_ROUND, SECOND_ROUND, THIRD_ROUND}
