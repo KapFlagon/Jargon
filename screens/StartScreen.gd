4b25c74c4f2ec48290c727fa4e57feeacdca443c
Jack Grimes <flangojack@hotmail.com>
2022-01-12 14:15:13 +0000

    Adding a basic wireframe for a start screen.

---
+++ screens/StartScreen.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_PlayBtn_button_down():
+	pass # Replace with function body.
+
+
+func _on_HighScoresBtn_button_down():
+	pass # Replace with function body.
+
+
+func _on_OptionsBtn_button_down():
+	pass # Replace with function body.
+
+
+func _on_SourceCodeBtn_button_down():
+	OS.shell_open("https://github.com/kapflagon/jargon")
